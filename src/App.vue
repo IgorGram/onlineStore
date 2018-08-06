df7407ead87f66ee392bc710a719b4d1f932b06d
Igor Grammatchikov <grammatchikov@inbox.ru>
2018-08-06 22:59:15 +0300

    add navbar

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app>
+       <Navbar/>
+    </v-app>
+</template>
+
+<script>
+    import Navbar from './components/Navbar'
+    export default {
+    	components: {
+			Navbar
+        }
+    }
+</script>
