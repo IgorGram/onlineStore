b16a4606069aa73dba7727a841d2dd97ad2543aa
Igor <grammatchikov@inbox.ru>
2018-08-15 17:13:52 +0300

    add login and reg by enter

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -2,7 +2,8 @@
     <v-container fluid fill-height>
         <v-layout align-center justify-center>
             <v-flex xs12 sm8 md4>
-                <v-card class="elevation-12">
+                <v-card class="elevation-12"
+                        @keyup.enter="onSubmit">
                     <v-toolbar dark color="primary">
                         <v-toolbar-title>Registration form</v-toolbar-title>
                     </v-toolbar>
